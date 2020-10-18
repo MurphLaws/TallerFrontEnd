054df7075a302c01df8e42b7f21c779e3a71db48
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2020-10-18 15:15:13 -0500

    initial commit

---
+++ connection.js
@@ -0,0 +1,20 @@
+
+const { Client } = require('pg');
+var express = require('express');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+const client = new Client({
+    user: 'xbnwykzk',
+    host: 'salt.db.elephantsql.com',
+    database: 'xbnwykzk',
+    password: 'dWHaA9Hy9Gt0dgmScmXHaJFeJ5A_-0UN',
+    port: 5432,
+  });
+client.connect();
+
+console.log("HOLA MUNDO")
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Escuchando en el puerto ${PORT}`));
