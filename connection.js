e1f2337a99ec40129c0af5bb51a2b4699570acf0
Nicolas <lasso.nicolas@correounivalle.edu.co>
2020-10-19 15:15:21 -0500

    Añadido styles.css

--- connection.js
+++ connection.js
@@ -23,13 +23,50 @@ app.post('/add',
     client.query('INSERT INTO pacientes (id, nombre, numid) VALUES(DEFAULT, $1, $2)', [req.body.n, req.body.numid],
             (e, r) => {
               console.log(e);
-              res.send('OK'); });
+              res.send('INSERTADO'); });
+    }
+);
+
+//UPDATE Customers SET ContactName = 'Alfred Schmidt', City= 'Frankfurt' WHERE CustomerID = 1;
+app.post('/update',
+ (req, res) => {
+    client.query('UPDATE pacientes SET nombre = $1, numid = $2 WHERE id = $3', [req.body.n, req.body.numid, req.body.i],
+            (e, r) => {
+              console.log(req.body);
+              res.send('ACTUALIZADO'); });
+    }
+);
+
+
+app.post('/total',
+ (req, res) => {
+   var data =  client.query('SELECT * FROM pacientes' ,
+            (e, r) => {
+              console.log(r.rows);
+              res.send(r.rows); });
     }
 );
 
 
 
+app.post('/read',
+ (req, res) => {
+   var data =  client.query('SELECT * FROM pacientes WHERE id=$1', [req.body.i],
+            (e, r) => {
+              console.log(r.rows);
+              res.send(r.rows); });
+    }
+);
 
 
+
+app.post('/delete',
+ (req, res) => {
+    client.query('DELETE FROM pacientes WHERE id = $1', [req.body.i],
+            (e, r) => {
+              console.log(e);
+              res.send('BORRADO'); });
+    }
+);
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Escuchando en el puerto ${PORT}`));
